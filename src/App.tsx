fa1948a847a06f3bb14ce46192a2dc3d8fb32b6c
Anto-MSHK <anjiilop25@gmail.com>
2022-04-29 00:06:49 +0300

    the data structure is ready and defined; debugging is needed

--- src/App.tsx
+++ src/App.tsx
@@ -34,17 +34,24 @@ type stydyDay = {
 		fifth: lesson | undefined
 	]
 }
-
+type lessonInfo = {
+	title?: string
+	type?: string
+	teacherInfo?: {
+		name: string
+		degree: string
+	}
+	cabinet?: string
+}
 type lesson = {
 	_debagProps: {
 		_fullСontentСell: string | undefined;
 	}
 	data: {
-		lessonName: string
-		teacherInfo: string
-		cabinet: string
-		time: string
-	} | undefined
+		topWeek: lessonInfo | undefined
+		lowerWeek?: lessonInfo | undefined
+		time?: string
+	}
 }
 
 type cell = {
@@ -111,7 +118,7 @@ function App() {
 
 			let letter: number = 0, num: number = 0;
 
-			if (workSheet[curKey].w && workSheet[curKey].w.length < 10) {
+			if (workSheet[curKey].w && workSheet[curKey].w.length < 15) {
 				for (let i = 0; i <= workSheet[curKey].w.length; i++) {
 					if (pattern.test(workSheet[curKey].w[i])) {
 						letter++;
@@ -121,7 +128,7 @@ function App() {
 				}
 			}
 
-			if (letter != 0 && num != 0 && letter < 5 && num < 5) {
+			if (letter != 0 && num != 0 && letter <= 5 && num <= 5) {
 				let referСell_number: string = '', referСell_letter: string = '';
 
 				for (let i = 0; i <= curKey.length; i++) {
@@ -136,12 +143,12 @@ function App() {
 				}
 				let correctColumn = checkingGroupCellIsCorrect(referСell_letter, +referСell_number + 1)
 				referСell_letter = correctColumn ? correctColumn : referСell_letter
-				allGroups[workSheet[curKey].w] = defineSchedule(referСell_letter, referСell_number, workSheet[curKey].w);
+				allGroups[workSheet[curKey].w] = defineSchedule(referСell_letter, referСell_number);
 			}
 		}
 	}
 
-	const defineSchedule = (referСell_letter: string, referСell_number: string, group: string) => {
+	const defineSchedule = (referСell_letter: string, referСell_number: string) => {
 		var stydyWeek: stydyWeek = {
 			_debagProps: { _referenceKey: undefined, _referenceKey_isСorrect: undefined },
 			days: [undefined, undefined, undefined, undefined, undefined, undefined]
@@ -176,21 +183,21 @@ function App() {
 		}
 
 		cellsOfDays.map((day: dayCells) => {
-			stydyWeek.days[iCurrentWeekDay] = defineDay(day, referСell_letter, referСell_number, group, iCurrentWeekDay)
+			stydyWeek.days[iCurrentWeekDay] = defineDay(day, referСell_letter)
 			iCurrentWeekDay++;
 		})
 
 		return stydyWeek;
 	}
 
-	const defineDay = (rangeCells: dayCells, referСell_letter: string, referСell_number: string, group: string, weekDay: number) => {
+	const defineDay = (rangeCells: dayCells, referСell_letter: string) => {
 		var stydyDay: stydyDay = {
 			lessons: [undefined, undefined, undefined, undefined, undefined,]
 		};
 
 		let iCurrentLesson = 0;
 
-		let column_lesson = alphabet[alphabet.indexOf(referСell_letter) - 1], column_cabinet = alphabet[alphabet.indexOf(referСell_letter) + 1]
+		let column_lesson = alphabet[alphabet.indexOf(referСell_letter) - 1]
 		let cellsOfLesson: dayCells[] = [{ i_cell_row_first: 0, i_cell_row_last: 0 }], i_lesson = 0
 		for (let i_cell = rangeCells.i_cell_row_first; i_cell <= rangeCells.i_cell_row_last; i_cell++) {
 			for (let i_cell_last = i_cell + 1; i_cell_last <= i_cell + 5; i_cell_last++)
@@ -208,28 +215,158 @@ function App() {
 		return stydyDay
 	}
 
-	const defineLesson = (referLesson: dayCells, referСell_letter: string) => {
-		let lesson: lesson = { _debagProps: { _fullСontentСell: '' }, data: undefined };
-		// referLesson += 1;
+	const getOtherInfo = (str: string): [nameTeacher: string, degree: string, typeLesson: string] => {
+		let surname = /^[А-Я][а-я]{1,20}\s[А-Я]\.[А-Я]\.$/
+		let nameTeacher: string = ''
+		let degree: string = ''
+		let typeLesson: string = ''
 
+		let i_degree: number = 0, i_type_lesson: number = 0;
 
-		for (let i_lesson = referLesson.i_cell_row_first; i_lesson <= referLesson.i_cell_row_last; i_lesson++) {
-			let currentValue: string | undefined = workSheet[referСell_letter + i_lesson] ? workSheet[referСell_letter + i_lesson].w : undefined;
-			if (checkingMerged(referСell_letter, i_lesson, referLesson.i_cell_row_last)) {
-				if (currentValue)
-					lesson._debagProps._fullСontentСell = currentValue + 'merge'
-				else
-					lesson._debagProps._fullСontentСell = 'void merge'
-				break
-			} else {
-				if (currentValue) {
-					lesson._debagProps._fullСontentСell += currentValue
-				} else {
-					lesson._debagProps._fullСontentСell += 'void';
+		for (let i_char_start = 0; i_char_start <= str.length; i_char_start++) {
+			let stop: boolean = false
+
+			for (let i_char_end = str.length; i_char_end >= 1; i_char_end--) {
+				let currentPhrase: string = '';
+
+				for (let i = i_char_start; i <= i_char_end; i++) {
+					currentPhrase += str[i]
 				}
+				if (surname.test(currentPhrase)) {
+					nameTeacher = currentPhrase
+					i_degree = i_char_start
+					i_type_lesson = i_char_end
+					stop = true
+				}
+			}
+
+			if (stop === true)
+				break
+		}
+
+		for (let i = 0; i <= i_degree - 1; i++) {
+			degree += str[i]
+		}
+
+		for (let i = i_type_lesson + 1; i <= str.length; i++) {
+			str[i] && (typeLesson += str[i])
+		}
+
+		return [nameTeacher, degree.trim(), typeLesson.trim()]
+	}
+
+	const getAllInfo = (str: string): [title: string, nameTeacher: string, degree: string, typeLesson: string] => {
+		let surname = /^[А-Я][а-я]{1,20}\s[А-Я]\.[А-Я]\.$/
+
+		let title: string = ''
+		let nameTeacher: string = ''
+		let degree: string = ''
+		let typeLesson: string = ''
+
+		let i_degree: number = 0, i_type_lesson: number = 0;
+
+		for (let i_char_start = 0; i_char_start <= str.length; i_char_start++) {
+			let stop: boolean = false
+
+			for (let i_char_end = str.length; i_char_end >= 1; i_char_end--) {
+				let currentPhrase: string = '';
+
+				for (let i = i_char_start; i <= i_char_end; i++) {
+					currentPhrase += str[i]
+				}
+				if (surname.test(currentPhrase)) {
+					nameTeacher = currentPhrase
+					i_degree = i_char_start
+					i_type_lesson = i_char_end
+					stop = true
+				}
+			}
+
+			if (stop === true)
+				break
+		}
+
+		let i_first_t = str.indexOf('.');
+		if (i_first_t != -1) {
+			for (let j = i_first_t; j >= 0; j--) {
+				if (str[j] === ' ') {
+					i_first_t = j;
+					break
+				}
+			}
+			for (let i = 0; i <= i_first_t; i++) {
+				title += str[i]
 			}
 		}
 
+		for (let i = i_first_t - 1; i <= i_degree - 1; i++) {
+			degree += str[i]
+		}
+
+		for (let i = i_type_lesson + 1; i <= str.length; i++) {
+			str[i] && (typeLesson += str[i])
+		}
+
+		return [title, nameTeacher, degree.trim(), typeLesson.trim()]
+	}
+
+	const defineLesson = (referLesson: dayCells, referСell_letter: string) => {
+		let lesson: lesson = { _debagProps: { _fullСontentСell: '' }, data: { topWeek: {} } };
+		// referLesson += 1;
+		let column_cabinet = alphabet[alphabet.indexOf(referСell_letter) + 1]
+
+		let currentLesson: string | undefined = workSheet[referСell_letter + referLesson.i_cell_row_first] ? workSheet[referСell_letter + referLesson.i_cell_row_first].w : undefined;
+		let currentTeacher: string | undefined = workSheet[referСell_letter + referLesson.i_cell_row_last] ? workSheet[referСell_letter + referLesson.i_cell_row_last].w : undefined;
+
+		if (checkingMerged(column_cabinet, referLesson.i_cell_row_first, referLesson.i_cell_row_last) && (currentLesson && currentTeacher)) {
+			let currentCabinet: string | undefined = workSheet[column_cabinet + referLesson.i_cell_row_first] ? workSheet[column_cabinet + referLesson.i_cell_row_first].w : undefined;
+
+
+			let otherData = currentTeacher ? getOtherInfo(currentTeacher) : [];
+			lesson.data.topWeek && (lesson.data.topWeek = {
+				title: currentLesson,
+				cabinet: currentCabinet,
+				teacherInfo: { name: otherData[0], degree: otherData[1] },
+				type: otherData[2]
+			})
+
+		} else {
+			let currentCabinetTop: string | undefined = workSheet[column_cabinet + referLesson.i_cell_row_first] ? workSheet[column_cabinet + referLesson.i_cell_row_first].w : undefined;
+			let currentCabinetLower: string | undefined = workSheet[column_cabinet + referLesson.i_cell_row_last] ? workSheet[column_cabinet + referLesson.i_cell_row_last].w : undefined;
+
+			let currentDataTop: string | undefined = workSheet[referСell_letter + referLesson.i_cell_row_first] ? workSheet[referСell_letter + referLesson.i_cell_row_first].w : undefined;
+			let currentDataLower: string | undefined = workSheet[referСell_letter + referLesson.i_cell_row_last] ? workSheet[referСell_letter + referLesson.i_cell_row_last].w : undefined;
+
+			let dataTop = currentDataTop ? getAllInfo(currentDataTop) : [];
+			let dataLower = currentDataLower ? getAllInfo(currentDataLower) : [];
+
+			if (currentDataTop)
+				lesson.data.topWeek = {
+					title: dataTop[0],
+					type: dataTop[3],
+					teacherInfo: {
+						name: dataTop[1],
+						degree: dataTop[2]
+					}
+				}
+			else
+				lesson.data.topWeek = undefined
+
+			if (currentDataLower)
+				lesson.data.lowerWeek = {
+					title: dataLower[0],
+					type: dataLower[3],
+					teacherInfo: {
+						name: dataLower[1],
+						degree: dataLower[2]
+					}
+				}
+			else
+				lesson.data.lowerWeek = undefined
+
+			lesson.data.topWeek && (lesson.data.topWeek.cabinet = currentCabinetTop ? currentCabinetTop : undefined);
+			lesson.data.lowerWeek && (lesson.data.lowerWeek.cabinet = currentCabinetLower ? currentCabinetLower : undefined);
+		}
 		return lesson;
 	}
 
@@ -252,60 +389,6 @@ function App() {
 		delete workSheet['!rows'];
 
 		defineGroups()
-
-
-
-		// var group: string[][] = [];
-		// for (let curKey in workSheet) {
-		// 	let letter: number = 0, num: number = 0;
-		// 	if (workSheet[curKey].w && workSheet[curKey].w.length < 10) {
-		// 		for (let i = 0; i <= workSheet[curKey].w.length; i++) {
-		// 			if (pattern.test(workSheet[curKey].w[i])) {
-		// 				letter++;
-		// 			}
-		// 			else if (patternNum.test(workSheet[curKey].w[i]))
-		// 				num++;
-		// 		}
-		// 	}
-		// 	if (letter != 0 && num != 0 && letter < 5 && num < 5) {
-		// 		group.push([workSheet[curKey].w, curKey]);
-		// 	}
-		// }
-
-		// group.map((el: any) => {
-		// 	console.log(el[0])
-
-		// 	let startNumVal: string = '', startStrVal: string = '';
-		// 	for (let i = 0; i <= el[1].length; i++) {
-		// 		if (patternEng.test(el[1][i])) {
-		// 			startStrVal += el[1][i]
-		// 		}
-		// 		else if (patternNum.test(el[1][i])) {
-		// 			startNumVal += el[1][i]
-		// 		}
-		// 		if (el[1].length - i === 1)
-		// 			break
-		// 	}
-		// 	for (let i = +startNumVal + 1; i <= +startNumVal + 10; i++) {
-		// 		let str: string | undefined = workSheet[startStrVal + i] ? workSheet[startStrVal + i].w : undefined;
-		// 		let yes: number = alphabet.indexOf(startStrVal) + 1;
-		// 		if ((str && str.length < 6) || (!str && workSheet[alphabet[yes] + i] && workSheet[alphabet[yes] + i].w.length > 6)) {
-		// 			let newStr = alphabet.indexOf(startStrVal);
-		// 			startStrVal = alphabet[newStr + 1];
-		// 			i = +startNumVal;
-		// 			// if (isMerged(startStrVal, i, workSheet['!merges']))
-		// 			// 	console.log('ЯЧЕЙКА ЕДИНА')
-		// 		} else {
-		// 			if (str) {
-		// 				console.log(str)
-		// 			}
-		// 			// if (isMerged(startStrVal, i, workSheet['!merges']))
-		// 			// 	console.log('ЯЧЕЙКА ЕДИНА')
-
-		// 		}
-		// 	}
-		// console.log(+startNumVal)
-		// })
 
 		console.log(allGroups)
 	}
